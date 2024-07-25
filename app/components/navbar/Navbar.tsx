cc39cb89281e11ae729b59e1cb850f9e2cfb6a7a
João Guedes <joao.guedes.developer@gmail.com>
2024-07-24 22:05:58 -0300

    estrutura base do projeto (frontend)

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="w-full fixed top-0 py-6 border-b bg-white z-10">
+        <div className="max-w-[1500px] mx-auto px-6">
+            <div className="flex justify-between items-center">
+                <Link href="/">
+                    <Image 
+                        src="/logo.png"
+                        alt="Logo"
+                        width={180}
+                        height={38}
+                    />
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
