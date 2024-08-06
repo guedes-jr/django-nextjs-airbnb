3718743135c2b44b83d4d7a13a7c5c10c548ad4f
João Guedes <joao.guedes.developer@gmail.com>
2024-08-05 23:34:03 -0300

    Minhas propriedades e a caixa de entrada concluídas até o momento

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,11 @@
+const Conversation = () => {
+    return (
+        <div className="px-6 py-4 border cursor-pointer border-gray-300 rounded-xl hover:shadow-xl">
+            <p className="mb-6 text-xl">John Guedes</p>
+
+            <p className="text-airbnb-dark">Ir para as conversas</p>
+        </div>
+    )
+}
+
+export default Conversation;
