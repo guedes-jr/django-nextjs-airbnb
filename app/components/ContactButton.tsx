0556820355476740fcbd8145a2edcb8b304f3c15
João Guedes <joao.guedes.developer@gmail.com>
2024-08-05 21:37:32 -0300

    Landlord detail page

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+    return (
+        <div className="mt-6 py-4 px-6 bg-airbnb text-white rounded-xl cursor-pointer hover:bg-airbnb-dark transition">
+            Contato
+        </div>
+    )
+}
+
+export default ContactButton;
